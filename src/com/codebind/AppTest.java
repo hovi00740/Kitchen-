799bcb81f73d3320f73c7a304c9448004e1b80cf
Your Name <you@example.com>
2017-03-27 10:11:43 -0400

    Commit

--- src/com/codebind/AppTest.java
+++ src/com/codebind/AppTest.java
@@ -1,6 +1,12 @@
 package com.codebind;
-
+import junit.framework.Assert;
 import junit.framework.TestCase;
+import java.sql.*;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+
 
 /**
  * Created by power on 3/27/17.
@@ -8,10 +14,16 @@ import junit.framework.TestCase;
 public class AppTest extends TestCase {
 
     public void testGetConnection() throws Exception {
-
+        Connection connection1, testConnection;
+        connection1 = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/chef","amd","drworm");
+        testConnection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/chef","amd","drworm");
+        assertEquals(connection1,testConnection);
     }
 
-    public void testGetUsersListChef() throws Exception {
+/*    public void testGetUsersListChef() throws Exception {
+        ArrayList<ChefsList> userList = new ArrayList<>();
+
+
 
     }
 
@@ -21,5 +33,5 @@ public class AppTest extends TestCase {
 
     public void testMain() throws Exception {
 
-    }
+    }*/
 }
